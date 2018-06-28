c3b12c762b2deecddfb2832adc6d5284a4607847
Jake-Jay <jake.jay.p@gmail.com>
2018-06-28 19:03:31 +0200

    Functionality of the server: 	- retrieve an unlabled pattern/image with the possible labels for that testing suite 	- register a device 	- send back a label for the image (connects the label, pattern and registered device)

--- src/labellingServer.py
+++ src/labellingServer.py
@@ -1,13 +1,14 @@
 #!/usr/local/bin/python3
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, Response
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from config import config
+import base64
 
 app = Flask(__name__)
 
-# ---- Not important (test function) - returns all 
+# ---- Not important (test function) - returns all labels for each of the training sets
 @app.route('/', methods=['GET'])
 def index():
     with connect() as conn:
@@ -16,21 +17,86 @@ def index():
         result = jsonify( query( conn, stmt ) )
     return result
 
-# ---- Retreive an unlabelled image using its image ID
-@app.route('/pattern/unlabelled/<int:pattern_id>', methods=['GET'])
-def unlabelled(pattern_id):
+# ---- User recieves an unlabelled image. Need to specifiy the image ID
+# * Need to modify to be able to actually retrieve an image rather than just the file type
+@app.route('/api/pattern/unlabelled', methods=['GET'])
+def unlabelled():
     with connect() as conn:
-        stmt = 'SELECT mime_type FROM pattern WHERE pattern_id=%d' %pattern_id
-        result =  query(conn, stmt)
-        print( result )
-        if result == []:
-            return( jsonify({'error':'pattern not found'}))
+        stmt = '''
+            WITH counts AS (
+                SELECT pattern_id, count(1) as occurrences
+                FROM phone_pattern_label
+                GROUP BY pattern_id
+            )
+            SELECT *
+            FROM pattern AS pp
+            LEFT OUTER JOIN counts as cc
+            ON pp.pattern_id = cc.pattern_id
+            WHERE cc.occurrences < 3 or cc.occurrences IS NULL
+            LIMIT 1
+            '''
+        pattern_result =  query(conn, stmt)
+        training_set_id =int( pattern_result[0]["training_set_id"] )
+        stmt = '''
+            SELECT label FROM labels
+            WHERE training_set_id = {}
+            '''.format(training_set_id)
+        label_result = query(conn, stmt)
+        if pattern_result == [] or label_result == []:
+            return( jsonify({'error':'pattern not found, or no labels found'}))
         else:            
-            return( jsonify(result))
+            return( jsonify({
+                'pattern_id': pattern_result[0]['pattern_id'],
+                'data': base64.encodebytes(pattern_result[0]['data']).decode("utf-8"),
+                'mime_type': pattern_result[0]['mime_type'],
+                'labels': list(l['label'] for l in label_result)
+             }) )
+
+# ---- User sends back proposed label. 
+# * Should send back the users imei, label. 
+# * Requires knowing the image ID and the potential labels
+@app.route('/api/pattern/<int:pattern_id>/label', methods=['POST'])
+def label(pattern_id):
+    # Perform data quality check
+    dev_num = request.json['device_number']
+    label = request.json['label']
+
+    with connect() as conn:
+        stmt = '''
+            INSERT INTO phone_pattern_label (pattern_id, phone_id, label_id)
+            SELECT pp.pattern_id, ph.phone_id, ll.label_id
+            FROM pattern as pp
+            INNER JOIN phones as ph
+            ON pp.pattern_id = {}
+            AND ph.device_number = \'{}\'
+            INNER JOIN labels as ll
+            ON ll.label = \'{}\'
+            '''.format(pattern_id, dev_num, label)
+        insert(conn, stmt)
+    return make_response('', 200)
+
+
+# ---- Function to add phones into the phone table
+@app.route('/api/register', methods=['POST'])
+def register():
+    imei = request.json['device_number']
+
+    if(validateDevice(imei)):
+        with connect() as conn:
+            stmt = '''
+                INSERT INTO phones(device_number)
+                VALUES({})
+                '''.format(imei)
+            insert(conn, stmt)
+            return make_response('Valid and unique imei - device registered succesfully \n', 200)         
+    else:
+        return make_response('Invalid imei \n', 500)
+    
 
 
 # ---- Add a potential label to a training set
-@app.route('/label/add_label/<int:training_set>', methods=['POST', 'GET'])
+# * Probably shouldnt be part of the API. Good way to test the server's communication with a database
+@app.route('/management/add_label/<int:training_set>', methods=['POST', 'GET'])
 def addPotentialLabel(training_set):
     if request.method == 'POST':
         if request.form: 
@@ -46,24 +112,29 @@ def addPotentialLabel(training_set):
         return( jsonify({'error': 'incorrect method'})) 
     
 
-# @app.route('/pattern/<int:pattern_id>/label', methods=['POST'])
-# def label(pattern_id):
-    # { 'label': 'dog', 'imea': '7q26rimq4y' }
-    #Request.data
-    # pass
+# ------------------------------------------------------------------------------------
+# Functions not bound to routes:
+# ------------------------------------------------------------------------------------
 
-# ---- Query function (also closes cursor connection)
+# ---- Query function for a general SELECT  (also closes cursor connection)
 def query(conn, stmt):
     # create a cursor which is used for select statements
     with conn.cursor(cursor_factory = RealDictCursor) as cur:
         cur.execute(stmt)
         result = cur.fetchall()
-        return(result)
+        return( result )
 
+# ---- For general insert statements
 def insert(conn, stmt):
     with conn.cursor(cursor_factory = RealDictCursor) as cur:
         cur.execute(stmt)        
     
+# ---- Validate imei - should contain 15 charachters (other rules?)
+def validateDevice(imei):
+    if (len(imei) == 15):
+        return True
+    else:
+        return False
 
 # ---- Returns a conection object to the Postgress DB
 def connect():
@@ -71,7 +142,6 @@ def connect():
     try:
         # read connection parameters from the .ini file
         params = config()
- 
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**params)
